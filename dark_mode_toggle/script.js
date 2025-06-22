5fa091462f94f94725587cafad0d7c86b1fecb71
Madhur <madhurbiradar21@gmail.com>
2025-06-22 23:46:12 +0530

    dark mode toggle done

--- dark_mode_toggle/script.js
+++ dark_mode_toggle/script.js
@@ -1,1 +1,15 @@
-!
+let text = document.querySelector("#text");
+let btn = document.querySelector("button");
+let isblack=true;
+
+btn.addEventListener("click",()=>{
+    if(isblack){
+        text.style.color="black"
+        document.body.style.backgroundColor="white";
+    }
+    else{
+        text.style.color="white"
+        document.body.style.backgroundColor="black"; 
+    }
+    isblack=!isblack;
+});
