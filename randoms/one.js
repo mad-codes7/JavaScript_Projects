ed989a892128d772472de7e501b1515cb32f6ce7
Madhur <madhurbiradar21@gmail.com>
2025-06-25 18:04:43 +0530

    array functions

---
+++ randoms/one.js
@@ -0,0 +1,75 @@
+// let arr = [1,2,3,4,5];
+
+// arr.forEach((arg)=>{
+//     console.log(arg);
+    
+// })
+
+// const arr = [{
+//     name :"madhur",
+//     marks:99
+// },{
+//     name :"aman",
+//     marks:98
+// },{
+//     name :"ajit",
+//     marks:97
+// }]
+
+// arr.forEach((e)=>{
+//     console.log(`MARKS of ${e.name} is ${e.marks}`);
+    
+// })
+
+
+
+//map
+
+// const arr = [1,2,3,4];
+// let arr1 = arr.map((e)=>{
+//     return e*e;
+// }) 
+// console.log(arr1);
+
+
+
+//filter
+
+// const arr = [1,2,3,4,5,6,7,8];
+// let arr2 = arr.filter((a)=>{
+//     return a%2==0
+// })
+
+// const arr = [1,2,3,4,5,6,7,8];
+// let arr2 = arr.filter((a)=>a%2==0)   //we can also use it without return
+
+// console.log(arr2);
+
+
+
+
+//every
+
+// let arr = [1,2,3,4]
+// let arr2 = [2,4,6,8]
+// const boolean = arr.every((e)=>e%2==0)
+// const newBoolean = arr2.every((e)=>e%2==0)
+
+// console.log(boolean);
+// console.log(newBoolean);
+
+
+
+// some
+
+// let arr = [1,2,3,4]
+// let arr2 = [2,4,6,8]
+// let arr3 = [1,3]
+// const boolean = arr.some((e)=>e%2==0)
+// const newBoolean = arr2.some((e)=>e%2==0)
+// const newBoolean2 = arr3.some((e)=>e%2==0)
+
+// console.log(boolean);
+// console.log(newBoolean);
+// console.log(newBoolean2);
+
